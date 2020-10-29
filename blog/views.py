344268bcc09a8a0951a0582233f4a740e00ca135
Nishant <nishant82@gmail.com>
2020-10-29 13:34:48 +0530

    added view and template for detailed blog post

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 
@@ -6,4 +6,9 @@ from django.utils import timezone
 def post_list(request):
 	#Get all the published post sorted/ordered by published date
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-	return render(request,'blog/post_list.html',{'posts' : posts})
+	return render(request, 'blog/post_list.html', {'posts' : posts})
+
+def post_detail(request , pk):
+	#post = Post.objects.get(pk=pk) -> can throw error if pk not found and we need to write our 404
+	post = get_object_or_404(Post, pk=pk)
+	return render(request, 'blog/post_detail.html', {'post' : post})
